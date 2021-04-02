6e406ae04f379bdff13353b8aab87a1e5e4121a6
DougWelchons <doug.welchons@gmail.com>
2021-04-02 13:36:45 -0600

    Add tests for items endpoint

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,21 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyString" }
-    unit_price { "9.99" }
-    merchat { nil }
+    sequence :id do |n|
+      "#{n}"
+    end
+
+    sequence :name do |n|
+      "Item#{n}"
+    end
+
+    sequence :description do |n|
+      "Desription#{n}"
+    end
+
+    sequence :unit_price do |n|
+      "#{n}.99"
+    end
+
+    merchant
   end
 end
