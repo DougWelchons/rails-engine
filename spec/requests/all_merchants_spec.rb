dd1d367fdfd3f74ab740b54539d31533f6522945
DougWelchons <doug.welchons@gmail.com>
2021-04-02 14:46:29 -0600

    fix tests to run with new configuration

--- spec/requests/all_merchants_spec.rb
+++ spec/requests/all_merchants_spec.rb
@@ -10,17 +10,17 @@ RSpec.describe "all merchants API end point" do
   it "returens merchants equal to the per_page value" do
     per_page = 10
     get "/api/v1/merchants?per_page=#{per_page}"
-    parsed = JSON.parse(response.body)
+    parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(response.status).to eq(200)
-    expect(parsed.count).to eq(per_page)
+    expect(parsed[:data].count).to eq(per_page)
   end
 
   it "returens 20 merchants by default" do
     get "/api/v1/merchants"
-    parsed = JSON.parse(response.body)
+    parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(response.status).to eq(200)
-    expect(parsed.count).to eq(20)
+    expect(parsed[:data].count).to eq(20)
   end
 end
