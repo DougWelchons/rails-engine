9b01b5318004d071eaa34a4d91e67d5196373e23
DougWelchons <doug.welchons@gmail.com>
2021-04-04 14:30:06 -0600

    Add testing for find_marchants end point

--- spec/requests/merchants_search_spec.rb
+++ spec/requests/merchants_search_spec.rb
@@ -4,13 +4,23 @@ RSpec.describe "merchants find_all API end point", type: :request do
   before :each do
     seed_test_db
   end
+   describe "Happy Path" do
+    it "returns all merchants who match the name search perams" do
+      name = "the"
+      get "/api/v1/merchants/find_all?name=#{name}"
 
-  it "returns all merchants who match the search peramiters" do
-    name = "the"
-    get "/api/v1/merchants/find_all?name=#{name}"
+      expect(response.status).to eq(200)
+      expect(json[:data].count).to eq(5)
+      expect(json[:data]).to be_an(Array)
+    end
 
-    expect(response.status).to eq(200)
-    expect(json[:data].count).to eq(5)
-    expect(json[:data]).to be_an(Array)
+    it "returns no objects if no merchants match the name search params" do
+      name = "No Match"
+      get "/api/v1/merchants/find_all?name=#{name}"
+
+      expect(response.status).to eq(200)
+      expect(json[:data].count).to eq(0)
+      expect(json[:data]).to be_an(Array)
+    end
   end
 end
