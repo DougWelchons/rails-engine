c9d82792e32a2ea8f411b04acf9b9a355f1c0573
DougWelchons <doug.welchons@gmail.com>
2021-04-05 14:39:38 -0600

    Add by revenue to the application record class

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.by_revenue(limit, object)
+    limit = 10 unless limit
+     joins(:transactions)
+    .where('transactions.result = ?', 'success')
+    .select("#{object}.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .group(:id)
+    .order(revenue: :desc)
+    .limit(limit)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Item, type: :model do
     end
     describe ".by_revenue" do
       it "returns 10 items by default sorted by revenue" do
-        items = Item.by_revenue(nil)
+        items = Item.by_revenue(nil, "items")
         expect(items.length).to eq(10)
         expect(items[0].name).to eq("Item 11")
         expect(items[1].name).to eq("Item 18")
@@ -31,7 +31,7 @@ RSpec.describe Item, type: :model do
 
       it "if a limit is provided, it will return items equal to the limit" do
         limit = 4
-        items = Item.by_revenue(limit)
+        items = Item.by_revenue(limit, "items")
         expect(items.length).to eq(limit)
         expect(items[0].name).to eq("Item 11")
         expect(items[1].name).to eq("Item 18")
@@ -41,7 +41,7 @@ RSpec.describe Item, type: :model do
 
       it "returns all items with successful transactions if limit is greater than the number of results" do
         limit = 50
-        items = Item.by_revenue(limit)
+        items = Item.by_revenue(limit, "items")
         expect(items.length).to eq(11)
         expect(items[0].name).to eq("Item 11")
         expect(items[1].name).to eq("Item 18")
